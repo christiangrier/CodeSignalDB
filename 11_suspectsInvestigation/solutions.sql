f5b9acf25fb37145e02af08a85a5e1830575e8e6
SumoSaiyan <62674125+SumoSaiyan@users.noreply.github.com>
2021-10-19 22:24:47 -0700

    Would you LIKE the Second Meal 11
    Added the first problem to the 3rd section

---
+++ 11_suspectsInvestigation/solutions.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE suspectsInvestigation()
+BEGIN
+    SELECT id, name, surname 
+    FROM Suspect
+    WHERE height <= 170 AND name LIKE 'B%' AND surname LIKE 'Gre_n'
+    ORDER BY id;
+END
