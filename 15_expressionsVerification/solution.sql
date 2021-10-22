6ff57c90803b83c2bb51961c0cdc41dc3abe0daf
SumoSaiyan <62674125+SumoSaiyan@users.noreply.github.com>
2021-10-22 15:22:34 -0700

    Would you LIKE the Second Meal 15
    Added solution to question 15

---
+++ 15_expressionsVerification/solution.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE expressionsVerification()
+BEGIN
+    SELECT *
+    FROM expressions 
+    WHERE 
+        CASE operation
+        WHEN '+' THEN a + b 
+        WHEN '-' THEN a - b 
+        WHEN '*' THEN a * b 
+        WHEN '/' THEN a / b
+        END = c 
+    ORDER BY id;
+END
