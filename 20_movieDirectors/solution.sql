9b04e678fc23bd77063bc460cefc1994eb30b011
SumoSaiyan <62674125+SumoSaiyan@users.noreply.github.com>
2021-10-23 20:23:30 -0700

    GROUP Dishes BY Type 20
    Added solution to question 20

---
+++ 20_movieDirectors/solution.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE movieDirectors()
+BEGIN
+    SELECT DISTINCT director 
+    FROM moviesinfo
+    WHERE year > 2000 
+    GROUP BY director
+    HAVING SUM(oscars) > 2
+    ORDER BY director ASC;
+END
